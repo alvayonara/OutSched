d2a061eaebbf676b83986362625c9030e4c9725e
Alva Yonara Puramandya <alvayonara@outlook.com>
2020-06-27 23:39:43 +0700

    Fix sort schedule date data

--- app/src/main/java/com/alvayonara/outsched/ui/schedule/SelectScheduleViewModel.kt
+++ app/src/main/java/com/alvayonara/outsched/ui/schedule/SelectScheduleViewModel.kt
@@ -14,6 +14,12 @@ import com.alvayonara.outsched.utils.ConvertUtils
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.text.DateFormat
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
 
 
 class SelectScheduleViewModel : ViewModel() {
@@ -62,12 +68,33 @@ class SelectScheduleViewModel : ViewModel() {
             }
         }
 
+        // Begin grouping schedule by date
         val consolidatedList = ArrayList<ScheduleListItem>()
 
         val groupedHashMap: HashMap<String, MutableList<ScheduleEntity>>? =
             groupDataIntoHashMap(listSchedules)
 
-        for (date in groupedHashMap!!.keys) {
+        // Sort hash map keys (date)
+        val sortedKeys: MutableList<String> =
+            ArrayList(groupedHashMap!!.size)
+        sortedKeys.addAll(groupedHashMap.keys)
+
+        Collections.sort(sortedKeys, object : Comparator<String?> {
+            // Parse key to date format as comparator
+            var convertToDate: DateFormat =
+                SimpleDateFormat("EEEE, dd MMMM yyyy", Locale.getDefault())
+
+            override fun compare(o1: String?, o2: String?): Int {
+                return try {
+                    convertToDate.parse(o1).compareTo(convertToDate.parse(o2))
+                } catch (e: ParseException) {
+                    throw IllegalArgumentException(e)
+                }
+            }
+        })
+
+        // Loop for each from sort key result
+        for (date in sortedKeys) {
             val dateItem = DateItem()
             dateItem.dateList = date
             consolidatedList.add(dateItem)
@@ -85,8 +112,9 @@ class SelectScheduleViewModel : ViewModel() {
     private fun groupDataIntoHashMap(listOfPojosOfJsonArray: List<ScheduleEntity>): HashMap<String, MutableList<ScheduleEntity>>? {
         val groupedHashMap: HashMap<String, MutableList<ScheduleEntity>> =
             HashMap()
+
         for (pojoOfJsonArray in listOfPojosOfJsonArray) {
-            val hashMapKey: String = ConvertUtils.convertTimeToDateFormat(pojoOfJsonArray.time)!!
+            val hashMapKey: String = ConvertUtils.convertTimeToDate(pojoOfJsonArray.time)!!
             if (groupedHashMap.containsKey(hashMapKey)) {
                 // The key is already in the HashMap; add the pojo object
                 // against the existing key.
@@ -98,6 +126,7 @@ class SelectScheduleViewModel : ViewModel() {
                 groupedHashMap[hashMapKey] = list
             }
         }
+
         return groupedHashMap
     }
 
